573397a7ddf4b8685adec6d6221d3cef703996b9
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2022-12-18 19:40:29 +0100

    packages updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { Navbar } from './components';
 
 const App = () => {
   return (
-    <div className="App">Albana's Dream</div>
+    <div className="App">
+		<Navbar />
+	</div>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./navbar/Navbar";
+
+export {
+	Navbar
+};
