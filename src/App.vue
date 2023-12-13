b87190225c977dcaf9524a5280a29dabfe839f7d
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2023-12-13 19:51:01 +0100

    c1

--- src/App.vue
+++ src/App.vue
@@ -40,14 +40,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
